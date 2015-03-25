67ccbbddc233a18691bbd11ee1fc9ff1074aeb68
YOKOENAOSUKE <yokoe@YOKOENAOSUKE-no-MacBook-Pro.local>
2015-03-25 18:43:03 +0900

    oreore fix
    * changed h1, h2, h3 and h4 font-size
    * open accordions of issue update form, always
    * add submit button to the top of issue update form

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -13,6 +13,13 @@
     $('#update fieldset.tabular legend').click(function() {
       $(this).closest('fieldset').toggleClass('show-children');
     });
+
+    var submit2 = $('<input name="commit" type="submit" value="送信" />');
+    submit2.css({"float": "right", "margin-right": "25px"});
+    submit2.on("click", function() {
+        $("#issue-form")[0].submit();
+    });
+    $('div.update-close').after(submit2);
   });
 
 })(jQuery);
